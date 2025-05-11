ad456517c57c5433213b2a97a20e389473fa0f39
AEndrix <redegalli.andrea03@gmail.com>
2025-05-12 01:47:15 +0200

    to ing

--- apps/printer-fe/src/app/pages/private/private.routes.ts
+++ apps/printer-fe/src/app/pages/private/private.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LayoutPrivateComponent } from '../../layout/private/layout-private.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const privateRoutes: Routes = [
   {
@@ -10,6 +11,10 @@ export const privateRoutes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
       },
     ],
   },

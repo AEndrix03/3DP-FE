821aba98e98d974f849860f08f8e0c850212b337
AEndrix <redegalli.andrea03@gmail.com>
2025-06-17 01:50:52 +0200

    chore: wip

--- apps/printer-fe/src/app/pages/private/slicing-result/slicing-result.component.ts
+++ apps/printer-fe/src/app/pages/private/slicing-result/slicing-result.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
   computed,
+  effect,
   signal,
   Signal,
   WritableSignal,
@@ -9,8 +10,12 @@ import { CommonModule, NgIf } from '@angular/common';
 import { PageTitleComponent } from '../../../core/components/shared/page-title/page-title.component';
 import { SlicingResultInfoComponent } from './slicing-result-info/slicing-result-info.component';
 import { SlicingResultTabsComponent } from './slicing-result-tabs/slicing-result-tabs.component';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ModelDetailPreviewComponent } from '../jobs/model-detail/model-detail-preview/model-detail-preview.component';
+import { SlicingResultDto } from '../../../core/models/slicing.models';
+import { SlicingService } from '../../../services/slicing.service';
+import { FileService } from '../../../services/file.service';
+import { map, switchMap, take, tap } from 'rxjs';
 
 @Component({
   selector: 'printer-slicing-result',
@@ -30,12 +35,19 @@ export class SlicingResultComponent {
     () => this.back() != null
   );
 
+  protected readonly slicingResult: WritableSignal<SlicingResultDto> =
+    signal(null);
   protected readonly sourceStlBlob: WritableSignal<Blob> = signal(null);
 
   private readonly fromRoute: WritableSignal<string | null> = signal(null);
   private readonly fromRouteId: WritableSignal<string | null> = signal(null);
 
-  constructor(private readonly router: Router) {
+  constructor(
+    private readonly router: Router,
+    private readonly actrivatedRoute: ActivatedRoute,
+    private readonly slicingService: SlicingService,
+    private readonly fileService: FileService
+  ) {
     this.back = computed(() =>
       this.fromRoute() == null
         ? null
@@ -50,5 +62,25 @@ export class SlicingResultComponent {
 
     this.fromRoute.set(state?.from as string | null);
     this.fromRouteId.set(state?.id as string | null);
+
+    this.actrivatedRoute.queryParams
+      .pipe(
+        take(1),
+        map((p) => p['id']),
+        switchMap((id) => this.slicingService.getSlicingResultById(id)),
+        tap((res) => this.slicingResult.set(res))
+      )
+      .subscribe();
+
+    effect(() => {
+      const res = this.slicingResult();
+
+      if (res != null) {
+        this.fileService
+          .downloadGlb(res.sourceId)
+          .pipe(tap((res) => this.sourceStlBlob.set(res)))
+          .subscribe();
+      }
+    });
   }
 }
